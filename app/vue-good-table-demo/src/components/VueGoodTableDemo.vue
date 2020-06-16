8a337b9149ac82420475a2a0bb8cae884cbc4b5c
Shuntaro Shimizu <shimizu@frankart.jp>
2020-06-16 13:48:59 +0900

    フィルタを実装

--- app/vue-good-table-demo/src/components/VueGoodTableDemo.vue
+++ app/vue-good-table-demo/src/components/VueGoodTableDemo.vue
@@ -23,29 +23,6 @@ export default {
   },
   data() {
     return {
-      columns: [
-        {
-          label: "Name",
-          field: "name"
-        },
-        {
-          label: "Age",
-          field: "age",
-          type: "number"
-        },
-        {
-          label: "Created On",
-          field: "createdAt",
-          type: "date",
-          dateInputFormat: "yyyy-MM-dd",
-          dateOutputFormat: "MMM Do yy"
-        },
-        {
-          label: "Percent",
-          field: "score",
-          type: "percentage"
-        }
-      ],
       rows: [
         {
           id: 1,
@@ -66,31 +43,96 @@ export default {
           name: "Susan",
           age: 16,
           createdAt: "2011-10-30",
-          score: 0.03343
+          score: 0.3342
         },
         {
           id: 4,
           name: "Chris",
           age: 55,
           createdAt: "2011-10-11",
-          score: 0.03343
+          score: 0.41571
         },
         {
           id: 5,
           name: "Dan",
           age: 40,
           createdAt: "2011-10-21",
-          score: 0.03343
+          score: 0.9045
         },
         {
           id: 6,
           name: "John",
           age: 20,
           createdAt: "2011-10-31",
-          score: 0.03343
+          score: 0.02901
         }
       ]
     };
+  },
+  computed: {
+    columns() {
+      return [
+        {
+          label: "Name",
+          field: "name",
+          filterOptions: {
+            enabled: true,
+            placeholder: "All",
+            filterDropdownItems: this.getDropdownItems("name")
+          }
+        },
+        {
+          label: "Age",
+          field: "age",
+          type: "number",
+          filterOptions: {
+            enabled: true,
+            placeholder: "All",
+            filterDropdownItems: this.getDropdownItems("age")
+          }
+        },
+        {
+          label: "Created On",
+          field: "createdAt",
+          type: "date",
+          dateInputFormat: "yyyy-MM-dd",
+          dateOutputFormat: "MMM Do yy",
+          filterOptions: {
+            enabled: true,
+            placeholder: "All",
+            filterDropdownItems: this.getDropdownItems("createdAt")
+          }
+        },
+        {
+          label: "Percent",
+          field: "score",
+          type: "percentage",
+          filterOptions: {
+            enabled: true,
+            placeholder: "All",
+            filterDropdownItems: this.getDropdownItems("score")
+          }
+        }
+      ];
+    }
+  },
+  methods: {
+    getDropdownItems(property) {
+      const array = Array.from(
+        new Set(this.rows.map(obj => obj[property]).sort())
+      );
+
+      if (property === "score") {
+        return array.map(value => {
+          return { value: value, text: this.formatScore(value) };
+        });
+      } else {
+        return array;
+      }
+    },
+    formatScore(score) {
+      return (score * 100).toFixed(2) + "%";
+    }
   }
 };
 </script>
